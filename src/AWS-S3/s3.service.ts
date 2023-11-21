7841e24995036bb719ae941b2d1ca7ae0a8b0a61
kaito <dung.hoang@codeenginestudio.com>
2023-11-21 21:12:41 +0700

    chore:enable CROS

---
+++ src/AWS-S3/s3.service.ts
@@ -0,0 +1,51 @@
+import { Injectable, BadRequestException } from '@nestjs/common';
+import { S3 } from 'aws-sdk';
+import { v4 as uuid } from 'uuid';
+
+@Injectable()
+export class S3Service {
+  async uploadImage(
+    image: Express.Multer.File,
+  ): Promise<{ key: string; url: string }> {
+    if (!image) throw new BadRequestException('Image is required');
+
+    if (!image?.mimetype.includes('image'))
+      throw new BadRequestException('This file must be an image');
+
+    const s3 = new S3();
+
+    const uploadResult = await s3
+      .upload({
+        Bucket: process.env.S3_BUCKET_NAME,
+        Body: image.buffer,
+        Key: `${uuid()}-${image.filename}`,
+        ContentType: image.mimetype,
+      })
+      .promise();
+
+    return {
+      key: uploadResult.Key,
+      url: uploadResult.Location,
+    };
+  }
+
+  async deleteImage(key: string): Promise<void> {
+    const s3 = new S3();
+    await s3
+      .deleteObject({
+        Bucket: process.env.S3_BUCKET_NAME,
+        Key: key,
+      })
+      .promise();
+  }
+
+  async generatePresignedUrl(key: string): Promise<string> {
+    const s3 = new S3();
+
+    return s3.getSignedUrlPromise('getObject', {
+      Bucket: process.env.S3_BUCKET_NAME,
+      Key: key,
+      Expires: 3600 * 3600,
+    });
+  }
+}
